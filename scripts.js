ccdebb54b6f43632b5cc96c3a5ddae71124d6852
chandlerdives <61131532+chandlerdives@users.noreply.github.com>
2022-09-05 21:40:51 +0100

    Carousel and 123 copywriting
    Writing out the "lets create something beautiful"

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-// accordion
-ar allPanels = $('.accordion > dd').hide();
+var allPanels = $('.accordion > dd').hide();
 
 	$('.accordion > dt').click(function() {
 		allPanels.slideUp();
@@ -12,11 +11,45 @@ ar allPanels = $('.accordion > dd').hide();
     return false;
 	});
 
+  // Animate on scroll
+ AOS.init({
+  offset: 250, // offset (in px) from the original trigger point
+  delay: 100, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+});
 
-  AOS.init();
+
+  $('.slider-container').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 
 
-  // hamburger button
+// hamburger button
   $('.hamburger-button').click(function(){
     $('.mobile-menu').slideToggle(100);
     $(this).toggleClass('active');
@@ -28,30 +61,3 @@ ar allPanels = $('.accordion > dd').hide();
 
 
 
-
-//   Slick
-$('.slider-container').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});

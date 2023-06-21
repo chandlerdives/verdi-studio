6e235734b0a8330960ccb003f4805e987d636295
chandlerdives <61131532+chandlerdives@users.noreply.github.com>
2023-06-21 19:46:32 +0200

    Header changes

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ var allPanels = $('.accordion > dd').hide();
   delay: 100, // values from 0 to 3000, with step 50ms
   duration: 1000, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
-  once: false // whether animation should happen only once - while scrolling down
+  once: true // whether animation should happen only once - while scrolling down
 });
 
 
